9b97c469ffb48bfbaafb98d8f9202b09eec1ac04
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-17 00:59:26 +0330

    feature(config): configure remote image patterns in `next.config.ts`
    Added support for remote image patterns in Next.js configuration to allow image loading from `cdn1.iconfinder.com` and `cloud.appwrite.io`. Improves compatibility with external image sources.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn1.iconfinder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
