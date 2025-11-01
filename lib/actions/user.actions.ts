b4c456d93577896fbe6d42bcab95357d5407a1da
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-01 12:37:08 +0330

    feature(auth): implement `signOutUser` function with session cleanup
    Added `signOutUser` function to handle user sign-out. Includes session deletion using `account.deleteSession`, cookie cleanup for `appwrite-session`, and redirection to `/sign-in`. Enhances authentication flow and user experience.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { Query, ID } from 'node-appwrite'
 import { parseStringify } from '@/lib/utils'
 import { avatarPlaceholderUrl } from '@/constants'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 interface createAccountParams {
   email: string
@@ -119,4 +120,17 @@ export const getCurrentUser = async () => {
   }
 
   return parseStringify(user.rows[0])
+}
+
+export const signOutUser = async () => {
+  const { account } = await createSessionClient()
+  try {
+    await account.deleteSession({ sessionId: 'current' })
+    const cookiesMethods = await cookies()
+    cookiesMethods.delete('appwrite-session')
+  } catch (e) {
+    handleError(e, 'Failed to sign out user')
+  } finally {
+    redirect('/sign-in')
+  }
 }
