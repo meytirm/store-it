dadc5464a2d837dd6db6bcfa2a074146ce9fe3aa
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-03 17:54:16 +0330

    feature(utils): add `getFileIcon` utility function
    Introduced a `getFileIcon` function to map file extensions and types to corresponding icon paths. Supports various file categories including documents, images, videos, audio, and others with default fallbacks. Enhances visual representation of files across the application.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { FileType } from 'next/dist/lib/file-exists'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -10,7 +11,7 @@ export function parseStringify(value: unknown) {
 }
 
 export function getFileType(fileName: string) {
-  const extension = fileName.toLowerCase().split('.').pop()
+  const extension = fileName.toLowerCase().split('.').pop() ?? ''
 
   const map: Record<string, string> = {
     // images
@@ -68,3 +69,65 @@ export function getFileType(fileName: string) {
 }
 
 export const convertFileToUrl = (file: File) => URL.createObjectURL(file)
+
+export const getFileIcon = (
+  extension: string | undefined,
+  type: FileType | string,
+) => {
+  switch (extension) {
+    // Document
+    case 'pdf':
+      return '/assets/icons/file-pdf.svg'
+    case 'doc':
+      return '/assets/icons/file-doc.svg'
+    case 'docx':
+      return '/assets/icons/file-docx.svg'
+    case 'csv':
+      return '/assets/icons/file-csv.svg'
+    case 'txt':
+      return '/assets/icons/file-txt.svg'
+    case 'xls':
+    case 'xlsx':
+      return '/assets/icons/file-document.svg'
+    // Image
+    case 'svg':
+      return '/assets/icons/file-image.svg'
+    // Video
+    case 'mkv':
+    case 'mov':
+    case 'avi':
+    case 'wmv':
+    case 'mp4':
+    case 'flv':
+    case 'webm':
+    case 'm4v':
+    case '3gp':
+      return '/assets/icons/file-video.svg'
+    // Audio
+    case 'mp3':
+    case 'mpeg':
+    case 'wav':
+    case 'aac':
+    case 'flac':
+    case 'ogg':
+    case 'wma':
+    case 'm4a':
+    case 'aiff':
+    case 'alac':
+      return '/assets/icons/file-audio.svg'
+
+    default:
+      switch (type) {
+        case 'image':
+          return '/assets/icons/file-image.svg'
+        case 'document':
+          return '/assets/icons/file-document.svg'
+        case 'video':
+          return '/assets/icons/file-video.svg'
+        case 'audio':
+          return '/assets/icons/file-audio.svg'
+        default:
+          return '/assets/icons/file-other.svg'
+      }
+  }
+}
