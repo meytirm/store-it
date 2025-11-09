6e11e33915012cf88e68548362b2f1232691a61c
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-09 18:39:15 +0330

    feature(ui): enhance `FileUploader` with file preview and removal
    Added support for file preview using a list with thumbnails and loading indicators. Implemented functionality to remove files from the upload queue. Enriched user experience by providing better visual feedback during uploads.

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,15 +1,23 @@
 'use client'
-import React, { useCallback } from 'react'
+import { useCallback, useState, type MouseEvent } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
+import { cn, convertFileToUrl, getFileType } from '@/lib/utils'
 import Image from 'next/image'
+import Thumbnail from '@/components/Thumbnail'
 
 function FileUploader({ ownerId, accountId, className }: Props) {
-  const onDrop = useCallback((acceptedFiles) => {
+  const [files, setFiles] = useState<File[]>([])
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles)
     // Do something with the files
   }, [])
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  function handleRemoveFile(e: MouseEvent<HTMLImageElement>, fileName: string) {
+    e.preventDefault()
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName))
+  }
 
   return (
     <div {...getRootProps()} className="cursor-pointer">
@@ -23,6 +31,44 @@ function FileUploader({ ownerId, accountId, className }: Props) {
         />
         <p>Upload</p>
       </Button>
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading...</h4>
+
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name)
+
+            return (
+              <li
+                key={`${file.name}-${index}`}
+                className="uploader-preview-item"
+              >
+                <div className="flex items-center gap-3">
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+                  <div className="preview-item-name">{file.name}</div>
+                  <Image
+                    src="/assets/icons/file-loader.gif"
+                    alt="loader"
+                    width={80}
+                    height={26}
+                  />
+                </div>
+                <Image
+                  src="/assets/icons/file-loader.gif"
+                  alt="Remove"
+                  height={26}
+                  width={26}
+                  onClick={(e) => handleRemoveFile(e, file.name)}
+                />
+              </li>
+            )
+          })}
+        </ul>
+      )}
       {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
