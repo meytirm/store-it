e656bd1161f3046884d2afe8ee221a517f4826a5
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-11 10:21:36 +0330

    feature(ui): add total size display to `Page` component
    Added a section in the `Page` component to display the total size with a placeholder value. Improves UI clarity by indicating the total size directly on the page.

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -6,6 +6,12 @@ async function Page({ params }: SearchParamProps) {
     <div className="page-container">
       <section className="w-full">
         <h1 className="h1 capitalize">{type}</h1>
+
+        <div className="total-size-section">
+          <p className="body-1">
+            Total size: <span className="h5">0 MB</span>
+          </p>
+        </div>
       </section>
     </div>
   )
