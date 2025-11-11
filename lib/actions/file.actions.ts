2dbf8d23bd722aefffd2a69a95e2662b37e9cc56
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-11 18:11:37 +0330

    feature(actions): add `getFiles` action to fetch user-specific files
    Implemented a new `getFiles` action in `file.actions.ts` to retrieve files associated with the current user. Includes query generation to filter files by ownership or shared access. Introduced helper methods for error handling and parsing file data.

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -3,13 +3,27 @@ import { UploadFileParams } from '@/types'
 import { createAdminClient } from '@/lib/appwrite'
 import { InputFile } from 'node-appwrite/file'
 import { appwriteConfig } from '@/lib/appwrite/config'
-import { ID } from 'node-appwrite'
+import { ID, Models, Query } from 'node-appwrite'
 import { constructFileUrl, getFileType, parseStringify } from '@/lib/utils'
 import { revalidatePath } from 'next/cache'
+import { getCurrentUser } from '@/lib/actions/user.actions'
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message)
   throw error
+}
+
+const createQueries = (currentUser: Models.User) => {
+  const queries = [
+    Query.or([
+      Query.equal('owner', [currentUser.$id]),
+      Query.contains('users', [currentUser.email]),
+    ]),
+  ]
+
+  // TODO: Search, sort, limits...
+
+  return queries
 }
 
 export const uploadFile = async ({
@@ -59,5 +73,28 @@ export const uploadFile = async ({
     return parseStringify(newFile)
   } catch (e) {
     handleError(e, 'Failed to upload file')
+  }
+}
+
+export const getFiles = async () => {
+  const { tables } = await createAdminClient()
+
+  try {
+    const currentUser = await getCurrentUser()
+    if (!currentUser) {
+      throw new Error('User not found')
+    }
+
+    const queries = createQueries(currentUser)
+
+    const files = await tables.listRows({
+      databaseId: appwriteConfig.databaseId,
+      tableId: 'files',
+      queries,
+    })
+    console.log(files)
+    return parseStringify(files)
+  } catch (e) {
+    handleError(e, 'Failed to get files')
   }
 }
