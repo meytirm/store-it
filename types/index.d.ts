62d9c290472bb95f13cc5b9410f0dd37c709553c
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-11 10:18:20 +0330

    feature(types): add `SearchParamProps` interface for typed search parameters
    Introduced a new `SearchParamProps` interface in `index.d.ts` to provide typing for `params` and `searchParams`. Facilitates consistent and type-safe handling of search parameters across the application.

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,3 +4,8 @@ export interface UploadFileParams {
   accountId: string
   path: string
 }
+
+declare interface SearchParamProps {
+  params?: Promise<SegmentParams>
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>
+}
