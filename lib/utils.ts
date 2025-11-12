e80c134ee44420311fe365eaeb604013e5c85dad
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-12 11:28:07 +0330

    feature(utils): add `constructDownloadUrl` helper function
    Introduced the `constructDownloadUrl` utility function to simplify generating download URLs for bucket files using environment variables. Enhances code reusability and maintainability.

--- lib/utils.ts
+++ lib/utils.ts
@@ -185,3 +185,7 @@ export const formatDateTime = (isoString: string | null | undefined) => {
 
   return `${time}, ${day} ${month}`
 }
+
+export const constructDownloadUrl = (bucketFileId: string) => {
+  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/download?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`
+}
