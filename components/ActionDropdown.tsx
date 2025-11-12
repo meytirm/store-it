dbcae430d930984be1e04866eedd8a00ed866cb1
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-12 11:45:18 +0330

    feature(ui): enhance `ActionDropdown` with icon triggers and dynamic actions
    Replaced text trigger with an icon button in `ActionDropdown` for improved UI aesthetics. Added support for dynamic actions, including modals for `rename`, `delete`, `share`, and `details`, and implemented download links with `constructDownloadUrl`. Simplified item rendering based on action type.

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FileInterface } from '@/types'
+import { ActionType, FileInterface } from '@/types'
 import { Dialog } from '@/components/ui/dialog'
 
 import {
@@ -12,21 +12,79 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { useState } from 'react'
+import Image from 'next/image'
+import { actionsDropdownItems } from '@/constants'
+import Link from 'next/link'
+import { constructDownloadUrl } from '@/lib/utils'
 
 function ActionDropdown({ file }: { file: FileInterface }) {
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  const [action, setAction] = useState<ActionType | null>(null)
+
   return (
     <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
       <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image
+            src="/assets/icons/dots.svg"
+            alt="dots"
+            width={34}
+            height={34}
+          />
+        </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuLabel className="max-w-[200px] truncate">
+            {file.name}
+          </DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
+          {actionsDropdownItems.map((item: ActionType) => (
+            <DropdownMenuItem
+              key={item.value}
+              className="shad-dropdown-item"
+              onClick={() => {
+                setAction(item)
+                if (action) {
+                  const isAllowed = [
+                    'rename',
+                    'delete',
+                    'share',
+                    'details',
+                  ].includes(action.value)
+
+                  if (isAllowed) {
+                    setIsModalOpen(true)
+                  }
+                }
+              }}
+            >
+              {item.value === 'download' ? (
+                <Link
+                  href={constructDownloadUrl(file.bucketFileId)}
+                  download={file.name}
+                  className="flex items-center gap-2"
+                >
+                  <Image
+                    src={item.icon}
+                    alt={item.label}
+                    width={30}
+                    height={30}
+                  />
+                  {item.label}
+                </Link>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={item.icon}
+                    alt={item.label}
+                    width={30}
+                    height={30}
+                  />
+                  {item.label}
+                </div>
+              )}
+            </DropdownMenuItem>
+          ))}
         </DropdownMenuContent>
       </DropdownMenu>
     </Dialog>
