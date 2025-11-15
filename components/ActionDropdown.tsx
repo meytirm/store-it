ca9c56d20bbfe082961b22bc2e982554dbfedd57
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-15 10:13:16 +0330

    feature(ui): add delete functionality to `ActionDropdown`
    Enabled file deletion in `ActionDropdown` by integrating the `deleteFile` function. Added logic to execute the delete action with required parameters like file ID, bucket file ID, and path.

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -14,7 +14,11 @@ import Loading from '@/components/Loading'
 import { usePathname } from 'next/navigation'
 import FileDropDownMenu from '@/components/FileDropDownMenu'
 import { FileDetails, ShareInput } from '@/components/ActionsModalContent'
-import { renameFile, updateFileUsers } from '@/lib/actions/file.actions'
+import {
+  deleteFile,
+  renameFile,
+  updateFileUsers,
+} from '@/lib/actions/file.actions'
 
 function ActionDropdown({ file }: { file: FileInterface }) {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -45,7 +49,8 @@ function ActionDropdown({ file }: { file: FileInterface }) {
           emails,
           path,
         }),
-      delete: () => console.log('delete'),
+      delete: () =>
+        deleteFile({ fileId: file.$id, bucketFileId: file.bucketFileId, path }),
     }
 
     success = await actions[action.value as keyof typeof actions]()
