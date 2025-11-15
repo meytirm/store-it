e387582c8a984d43baa42a93958d2c6d3810e6b6
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-15 16:44:05 +0330

    feature(ui): enhance `Search` component with dynamic results
    Added dynamic file search results to the `Search` component using `getFiles`. Implemented query-based state management and integrated support for showing search results with thumbnails, file names, and formatted dates. Improved user interaction and search functionality.

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,10 +1,34 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { Input } from '@/components/ui/input'
+import { useSearchParams } from 'next/navigation'
+import { getFiles } from '@/lib/actions/file.actions'
+import { FileInterface } from '@/types'
+import Thumbnail from '@/components/Thumbnail'
+import FormattedDateTime from '@/components/FormattedDateTime'
 
 function Search() {
   const [query, setQuery] = useState('')
+  const searchParms = useSearchParams()
+  const searchQuery = searchParms.get('query') || ''
+  const [results, setResults] = useState<FileInterface[]>([])
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const files = await getFiles({ searchText: query })
+      setResults(files.rows)
+      setOpen(true)
+    }
+
+    fetchFiles()
+  }, [query])
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery('')
+    }
+  }, [searchQuery])
 
   return (
     <div className="search">
@@ -21,6 +45,37 @@ function Search() {
           className="search-input"
           onChange={(e) => setQuery(e.target.value)}
         />
+
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file: FileInterface) => (
+                <li
+                  className="flex items-center justify-between"
+                  key={file.$id}
+                >
+                  <div className="flex cursor-pointer items-center gap-3">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No files found</p>
+            )}
+          </ul>
+        )}
       </div>
     </div>
   )
