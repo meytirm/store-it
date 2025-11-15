fc22d48c8dc60fed378f630cc7eb085e4b005d6f
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-15 15:14:42 +0330

    feature(ui): enhance `Search` component with input and icon
    Added interactive search input box and search icon to the `Search` component. Introduced state management for query handling, enabling dynamic user input. Improved styling and accessibility.

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,29 @@
+'use client'
+import { useState } from 'react'
+import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+
 function Search() {
-  return <div>search</div>
+  const [query, setQuery] = useState('')
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="Search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search..."
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Search
