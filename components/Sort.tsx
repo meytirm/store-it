45df3062805029ada695ee7cec9c5e47dc750b2b
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-16 11:34:31 +0330

    feature(ui): implement `Sort` component with dynamic routing and select options
    Added a new `Sort` component using Radix UI's `Select`. Implemented dynamic routing with `usePathname` and `useRouter` to handle sort parameter updates in the URL. Integrated `sortTypes` for defining selectable sort options. Enhanced user interaction with seamless sorting functionality.

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,5 +1,39 @@
+'use client'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import { usePathname, useRouter } from 'next/navigation'
+import { sortTypes } from '@/constants'
+
 function Sort() {
-  return <div>sort</div>
+  const path = usePathname()
+  const router = useRouter()
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`)
+  }
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.value}
+            value={sort.value}
+            className="shad-select-item"
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  )
 }
 
 export default Sort
